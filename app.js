1fed87478ba90406f51defcce9aa7c22d176c960
Andrej Kyselica <andrej@kyselica.com>
2017-06-09 13:57:23 -0400

    test env

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.use('/env', function (req, res) {
+  var envText =
+      process.env.map(function (env) {
+      return env; 
+    }).join('/');
+  res.send('env: ' + envText);
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
